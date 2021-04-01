a21d1f64b50eb12f1bd59674fe5261aa557d93f5
DonghoonPark <donghoon.park@kohyoung.com>
2021-04-01 17:19:51 +0900

    first commit with simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import { Nav, Navbar } from 'react-bootstrap';
+import {AiFillCalculator} from 'react-icons/ai'
+import {FaCalculator} from 'react-icons/fa'
 
 function App() {
+  let link_style : CSSProperties = {
+    textDecoration : 'inherit',
+    color : 'inherit'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand> <FaCalculator/> Type Calculator </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link><Link to="/float32" style={link_style}>Float32</Link></Nav.Link>
+              <Nav.Link><Link to="/float64" style={link_style}>Float64</Link></Nav.Link>
+              <Nav.Link><Link to="/qfixed"  style={link_style}>Q-Fixed</Link></Nav.Link>
+              <Nav.Link><Link to="/fixed_arithmetic" style={link_style}>Fixed Arithmetic</Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route path="/float32"></Route>
+          <Route path="/float64"></Route>
+          <Route path="/qfixed"></Route>
+          <Route path="/fixed_arithmetic"></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
